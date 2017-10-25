2695af06878520b7428baae89ebb66d0ce0ee793
Karmila-Ismydewi@dKarmila <dcarmy.karmila@gmail.com>
2017-10-25 15:36:28 +0700

    edit cors index.js app.use(function(req, res, next){     res.header("Access-Control-Allow-Origin", "*");     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");     next(); });

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ require('./config/configdb');
 
 let app = express();
 
-app.use(function(res, req, next){
-    res.header("Access-Control-Allow-Access", "*");
+app.use(function(req, res, next){
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
     next();
