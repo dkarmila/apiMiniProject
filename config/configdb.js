f1245c45543edaf84b8521aeed2e19d3daf39df5
Karmila-Ismydewi@dKarmila <dcarmy.karmila@gmail.com>
2017-10-25 12:51:39 +0700

    first commit

--- config/configdb.js
+++ config/configdb.js
@@ -6,12 +6,12 @@ let database ={
     url : 'ds229835.mlab.com:29835',
     name : 'dbelektronik'
 }
-mongoose.connect("mongodb://"+database.user+":"+database.pass+"@"+database.url+"/"+database.name);
-
+//mongoose.connect("mongodb://"+database.user+":"+database.pass+"@"+database.url+"/"+database.name);
+mongoose.connect('mongodb://'+ database.user +':'+ database.pass +'@'+ database.url +'/'+ database.name);
 //mongoose.connect('mongodb://localhost:27017/DBElektronik');
 
-db=mongoose.connection;
-db.on('error', console.log.bind(console, 'Connection Error..'));
+let db=mongoose.connection;
+db.on('error', console.error.bind(console, "Connection Error.."));
 db.once('open', function(){
     console.log("Connection to dbElektronik is open ..");
 })
