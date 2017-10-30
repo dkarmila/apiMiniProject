4f477850525621b6d2c1e8cfa99f739f129c0617
Karmila-Ismydewi@dKarmila <dcarmy.karmila@gmail.com>
2017-10-30 09:16:06 +0700

    no message

--- config/configdb.js
+++ config/configdb.js
@@ -1,14 +1,13 @@
 let mongoose = require('mongoose');
 
-// let database ={
-    // user : 'dbelektronik',
-    // pass : 'miniprojek',
-    // url : 'ds229835.mlab.com:29835',
-    // name : 'dbelektronik'
-// }
-// //mongoose.connect("mongodb://"+database.user+":"+database.pass+"@"+database.url+"/"+database.name);
-// mongoose.connect('mongodb://'+ database.user +':'+ database.pass +'@'+ database.url +'/'+ database.name);
-mongoose.connect('mongodb://localhost:27017/DBElektronik');
+let database ={
+    user : 'dbelektronik',
+    pass : 'miniprojek',
+    url : 'ds229835.mlab.com:29835',
+    name : 'dbelektronik'
+}
+mongoose.connect('mongodb://'+ database.user +':'+ database.pass +'@'+ database.url +'/'+ database.name);
+//mongoose.connect('mongodb://localhost:27017/DBElektronik');
 
 let db=mongoose.connection;
 db.on('error', console.error.bind(console, "Connection Error.."));
